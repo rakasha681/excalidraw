e1ad5281ad76bc05ecd8f5bde9d85abe51eb3575
rakasha681 <30880020+rakasha681@users.noreply.github.com>
2025-04-07 21:41:24 -0400

    Update index.tsx

--- excalidraw-app/index.tsx
+++ excalidraw-app/index.tsx
@@ -268,7 +268,7 @@ export const appLangCodeAtom = atom(
 const ExcalidrawWrapper = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [langCode, setLangCode] = useAtom(appLangCodeAtom);
-  const isCollabDisabled = isRunningInIframe();
+  const isCollabDisabled = false;
 
   // initial state
   // ---------------------------------------------------------------------------
