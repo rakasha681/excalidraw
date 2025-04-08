504ed57233a12a35603f23c50de63907a8ad70ba
rakasha681 <30880020+rakasha681@users.noreply.github.com>
2025-04-07 21:39:11 -0400

    Update index.tsx

--- excalidraw-app/index.tsx
+++ excalidraw-app/index.tsx
@@ -110,18 +110,6 @@ window.EXCALIDRAW_THROTTLE_RENDER = true;
 
 let isSelfEmbedding = false;
 
-if (window.self !== window.top) {
-  try {
-    const parentUrl = new URL(document.referrer);
-    const currentUrl = new URL(window.location.href);
-    if (parentUrl.origin === currentUrl.origin) {
-      isSelfEmbedding = true;
-    }
-  } catch (error) {
-    // ignore
-  }
-}
-
 const languageDetector = new LanguageDetector();
 languageDetector.init({
   languageUtils: {},
